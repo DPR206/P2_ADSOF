7dd7f103e07159ab59f3aef09e78e22736991536
claudsze <clasaiz@outlook.es>
2026-02-09 18:32:29 +0100

    documentacion y tiempo en tren

--- TramoTrayecto.java
+++ TramoTrayecto.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Este paquete contiene las clases necesarias para la gestión de los trayectos a pie y a tren
  */
 package trayectos;
 
@@ -14,18 +14,34 @@ public abstract class TramoTrayecto {
 	private String origen;
 	private String destino;
 	
+  /**
+   * Crea un nuevo TramoTrayecto
+   * 
+   * @param origen origen del trayecto
+   * @param destino destino del trayecto
+   * @return nuevo TramoTrayecto creado
+   */
 	public TramoTrayecto(String origen, String destino) {
 		this.origen = origen;
 		this.destino = destino;
 	}
 	
+  /**
+   * Devuelve el tiempo que se tarda en recorrer un trayecto
+   * 
+   * @return tiempo en recorrer un trayecto
+   */
 	public double tiempo() {
 		return 0.0;
 	}
 
+  /**
+   * Devuelve la información de un trayecto en cadena de texto
+   * 
+   * @return String con información de trayecto
+   */
 	@Override
 	public String toString() {
 		return "desde "+this.origen+" a "+this.destino+": "+this.tiempo()+" minutos";
 	}
 }
-
--- TramoTren.java
+++ TramoTren.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * Este paquete contiene las clases necesarias para la gestión de los trayectos a pie y a tren
  */
 package trayectos;
 
@@ -14,12 +14,50 @@ public class TramoTren extends TramoTrayecto {
 	private Linea linea;
 	private int numParadas;
 	
+  /**
+   * Crea un nuevo TramoTren
+   * 
+   * @param origen origen del tramo
+   * @param destino destino del tramo
+   * @param linea linea de tren del trayecto
+   * @param numParadas numero de paradas del tramo
+   * @return nuevo TramoTren creado
+   */
 	public TramoTren(String origen, String destino, Linea linea, int numParadas) {
 		super(origen, destino);
 		this.linea =linea;
 		this.numParadas = numParadas;
 	}
+
+  /**
+   * Devuelve el tiempo en recorrer un trayecto en tren
+   * 
+   * @return double, tiempo en recorrer el trayecto
+   */
+  public double tiempo() {
+    t = this.numParadas;
+    
+    switch (this.linea) {
+      case C1:
+        t *= 5;
+        break;
+      case C4:
+        t *= 10;
+        break;
+      case C5:
+        t *= 30;
+        break;
+      default:
+    }
+    
+    return t;
+  }
 	
+  /**
+   * Devuelve la información del TramoTren en cadena de texto
+   * 
+   * @return String con la información
+   */
 	@Override
 	public String toString() {
 		return "En tren de la línea "+this.linea+" "+super.toString();
