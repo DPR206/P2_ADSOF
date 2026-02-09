c2f3ffb851d8e7529287245249722e03952ae12d
claudsze <clasaiz@outlook.es>
2026-02-09 18:06:50 +0100

    documentacion

--- Linea.java
+++ Linea.java
@@ -1,15 +1,37 @@
+/**
+ * Este paquete contiene las clases necesarias para la gestión de los trayectos a pie y a tren
+ */
 package trayectos;
+
+/**
+ * Esta clase es una enumeración de líneas de tren
+ * @author Claudia Saiz Escribano y Duna Puente Romera. 
+ * @version 1.0
+ * Nombre del fichero: Linea.java
+ */
 public enum Linea{
-  C1("azul claro");
-  C4("azul oscuro");
+  C1("azul claro"),
+  C4("azul oscuro"),
   C5("amarilla");
 
   private String color;
 
+  /**
+   * Crea una nueva línea
+   * 
+   * @param color color de la linea
+   * @return nueva linea creada
+   */
   private Linea(String color) {
     this.color = color;
   }
 
+  /**
+   * Devuelve la información de la linea como cadena de texto
+   * 
+   * @return String con información de la linea
+   */
+  @Override
   private String toString() {
     return this.name() + " (" + this.color + ") ";
   }
--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+/**
+ * Este paquete contiene las clases necesarias para la gestión de los trayectos a pie y a tren
+ */
 package trayectos;
 
+/**
+ * Esta clase prueba las funcionalidades de las clases TramoTrayecto, TramoTren, TramoAPie, y Linea
+ * @author Claudia Saiz Escribano y Duna Puente Romera. 
+ * @version 1.0
+ * Nombre del fichero: Main.java
+ */
 public class Main {
+
+  /**
+   * Punto de entrada de la aplicación
+   * 
+   * @param args Argumentos de la línea de comandos
+   */
   public static void main(String[] args) {
     TramoTrayecto[] trayecto = {
       new TramoAPie ("Hotel Puerta del Sol", "Sol Renfe", 1),
